f22452371aed4de4e203fecb3b672e20be8679e0
aloes2512 <alf.loeffler@icloud.com>
2022-03-01 22:13:33 +0100

    Sample coade to try multivariant prognos

---
+++ mgcViz_example.R
@@ -0,0 +1,51 @@
+# Forecast with mgcViz
+library(mgcViz)
+library(tidyverse)
+# example code
+n  <- 1e3
+dat <- data.frame("x1" = rnorm(n), "x2" = rnorm(n), "x3" = rnorm(n))
+dat$Y <- with(dat, sin(x1) + 0.5*x2^2 + 0.2*x3 + pmax(x2, 0.2) * rnorm(n))
+range(dat$x1)
+dat$t<- seq(min(dat$x1),max(dat$x1),length.out =n)
+dat<-dat%>% mutate(z= sin(t))
+ dat%>% ggplot(aes(x= x1,y=Y))+
+  geom_point(col= "red",size = 0.5)+
+  geom_point(aes(x=x2,y=Y),col = "green",size=0.5)+
+   geom_line(data =dat,mapping =aes(x=t,y=z))
+b <- gam(Y ~ s(x1) + s(x2) + x3, data = dat, method = "REML")
+b <- getViz(b)
+# first smooth function
+sm(b,1)
+o <- plot( sm(b, 1) )
+o<-o+geom_line(data = dat,mapping=(aes(x=t,y=z)),linetype= 1,col ="red")
+o + l_fitLine(colour = "red") + l_rug(mapping = aes(x=x, y=y), alpha = 0.8) +
+  l_ciLine(mul = 5, colour = "blue", linetype = 3) + 
+  l_points(shape = 19, size = 1, alpha = 0.1) + theme_classic()
+# data from "Freight_forecast.R" 
+my_dat <- KBA_dfr
+head(my_dat)# Land       Jahr Miotkm
+my_mdl<-gam(Miotkm ~ s(Jahr),data= my_dat, method = "REML")
+
+summary(my_mdl)
+my_b<- getViz(my_mdl)
+my_o<-plot(sm(my_b,1))
+listLayers(my_o)
+my_o+l_fitLine(colour = "red") + l_rug(mapping = aes(x=x, y=y)) +
+  l_ciLine(mul = 5, colour = "blue", linetype = 2) + 
+  l_points(shape = 3, size = 1, alpha = 0.5) + 
+  theme_classic()
+
+plt_0<- KBA_mdl%>%ggplot()+
+  geom_point(aes(x= Jahr,y= Miotkm))+
+  geom_smooth(aes(x= Jahr,y= .fitted))+
+  geom_point(aes(x= Jahr,y= .fitted),col= "blue")+
+  labs(x= "Jahr")+
+  ggtitle("Freight (road-transport)
+          Germany",
+          subtitle="KBA-data & Trend")
+KBA_mdl%>% scale()%>% as_tibble()%>% mutate(Jahr = 2010:2020)%>%ggplot()+
+  geom_point(aes(x= Jahr,y= Miotkm))+
+  geom_smooth(aes(x= Jahr,y= .fitted))+
+  geom_point(aes(x= Jahr,y= .fitted),col= "blue")+
+  labs(x= "Jahr")
+plot(sm(my_b,1))+geom_point(data = my_dat, aes(x= Jahr, y= Miotkm/10^3))
